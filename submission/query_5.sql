24471e719ba9b70ee7f109b757e010804f26bb32
Troy D <134393565+troydueck@users.noreply.github.com>
2024-05-21 09:11:27 -0400

    Update query_5.sql
    Q5 initial submission

---
+++ submission/query_5.sql
@@ -0,0 +1,9 @@
+CREATE OR REPLACE TABLE tdueck66966.hosts_cumulated (
+  host VARCHAR, --'host': stores the host from the web_events dataset.
+  host_activity_datelist ARRAY(DATE), --'host_activity_datelist': stores the array of event activity for the host from the web_events dataset.
+  date DATE --'date': stores the capture date of the user web activity to build a cumulative table.
+)
+WITH (
+  FORMAT = 'PARQUET',
+  partitioning = ARRAY['date']
+)
