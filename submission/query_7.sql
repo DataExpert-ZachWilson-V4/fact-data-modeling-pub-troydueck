4dd397871bfaf12ad114d87db7300b14a34ef487
Troy D <134393565+troydueck@users.noreply.github.com>
2024-05-21 10:02:39 -0400

    Update query_7.sql

--- submission/query_7.sql
+++ submission/query_7.sql
@@ -2,7 +2,7 @@ CREATE OR REPLACE TABLE tdueck66966.monthly_host_activity_reduced (
   host VARCHAR, --'host': stores the host site from the web_events dataset.
   metric_name VARCHAR, --'metric_name': stores the derived value for the metric evaluated from the full url path in the web_events dataset.
   metric_array ARRAY(INTEGER), --'metric_array': stores the array of web activity for the given url path for each day of the month.
-  month_start VARCHAR --'month_start': stores the first day of the month to be used as the fixed date to analyze the array.
+  month_start VARCHAR --'month_start': stores the first day of the month to be used as the fixed date to analyze the array
 )
 WITH
 (
