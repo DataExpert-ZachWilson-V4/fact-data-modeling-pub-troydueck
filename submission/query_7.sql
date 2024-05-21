a84a3621c81c0040532466d167fcd0a6e1423b77
Troy D <134393565+troydueck@users.noreply.github.com>
2024-05-21 09:10:48 -0400

    Update query_7.sql
    Cancelling commit from 'main' branch

--- submission/query_7.sql
+++ submission/query_7.sql
@@ -1,11 +1,1 @@
-CREATE OR REPLACE TABLE tdueck66966.monthly_host_activity_reduced (
-  host VARCHAR,
-  metric_name VARCHAR,
-  metric_array ARRAY(INTEGER),
-  month_start VARCHAR
-)
-WITH
-  (
-    FORMAT = 'PARQUET',
-    partitioning = ARRAY['metric_name', 'month_start']
-  )
+
